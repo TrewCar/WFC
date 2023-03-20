84954e0edd1f348f82de70ba91504a0f927b63ec
TrewCar <113162792+TrewCar@users.noreply.github.com>
2023-03-20 21:05:48 +0300

    Без изменения входного изображения, не будет повторно изменён список тайлов. UI потом

--- FORM/Form1.cs
+++ FORM/Form1.cs
@@ -15,24 +15,27 @@ public partial class Form1 : Form
             Point pos = new Point(e.X / 5, e.Y / 5);
             g.FillRectangle(new SolidBrush(pen), pos.X * 5, pos.Y * 5, 5, 5);
             pictureBox1.Invalidate();
+            
         }
     }
 
     private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
     {
         Draw = true;
+        UpdatePicture = false;
     }
     Color pen = Color.FromArgb(50, 50, 50);
     private void Button1_Click(object sender, EventArgs e)
     {
         pen = Color.FromArgb(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
     }
-
+    private bool UpdatePicture = false;
     private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
     {
         Draw = false;
+        UpdatePicture = false;
     }
-    List<Tile> Tiles = new();
+    List<Tile> Tiles;
     private void Button2_Click(object sender, EventArgs e)
     {
         Bitmap pic;
@@ -47,8 +50,10 @@ public partial class Form1 : Form
             }
             try
             {
-                List<Tile> Tiles = CreateGrid.CreateTile((Bitmap)pictureBox1.Image);
-
+                if (!UpdatePicture)
+                {
+                    Tiles = CreateGrid.CreateTile((Bitmap)pictureBox1.Image);
+                }
                 var t = new WFC(Tiles, pictureBox2.Width, pictureBox2.Height);
                 pic = t.CreateIMG();
                 pictureBox2.Image = pic;
@@ -60,5 +65,6 @@ public partial class Form1 : Form
                 continue;
             }
         }
+        UpdatePicture = true;
     }
 }
